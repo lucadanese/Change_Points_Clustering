265c3e3912d705f332785d92bc06b4b4f18c03f3
Luca Danese <46844267+lucadanese@users.noreply.github.com>
2024-07-15 13:00:26 +0200

    Update EPI03_run_simulations.R

--- EPI03_run_simulations.R
+++ EPI03_run_simulations.R
@@ -7,7 +7,7 @@ sourceCpp(file = "EPI01_main_code.cpp")
 # run the following line if data have not been generated yet
 #source("EPI02_generate_simdata.R") 
 
-path <- "G:\\My Drive\\Dottorato\\Progetti\\Model Based Clustering of TS\\Simulazioni_EPI\\data\\simEPIdata_24_05_2024" # path with the simulated EPI data  
+path <- "" # path with the simulated EPI data  
 load(path)
 
 ##--SET PARAMETERS--##
@@ -25,23 +25,18 @@ sample_prop <- 0.20 # set proportion of sampled infection times
 set.seed(123)
 
 for(r in 1:1){
-  
   for(rep in 1:50){
 
     y <- data_INIT
     
     y_rounded_pre <- lapply(y, function(x) table(sample(ceiling(x), sample_prop * length(x), replace = FALSE)))
-    
     y_rounded <- list()
-    
     for(i in 1:length(y_rounded_pre)){
       y_rounded[[i]] <- rep(0, length(seq(1,maxT)))
       names(y_rounded[[i]]) <- seq(1,maxT)
       y_rounded[[i]][as.numeric(names(y_rounded_pre[[i]]))] <- as.numeric(y_rounded_pre[[i]])
     }
-    
     data_sim <- matrix(NA, 10, maxT)
-    
     for(i in 1:length(y_rounded)){
       data_sim[i,1:200] <- y_rounded[[i]]
     }
@@ -59,12 +54,10 @@ for(r in 1:1){
                                S0 = 1, R0 = 0, 
                                p = 0.005, nupd = 250)
 	
-	  path_to_save <- "G:\\My Drive\\Dottorato\\Progetti\\Model Based Clustering of TS\\Simulazioni_EPI\\11_07_2024\\"
+    path <- ""
     
-    save(est_model, file = paste0(path_to_save,"est_sim_EPI_", "prova", "_", par_mcrep, "_", par_nsplitmerge, "_", par_R,"_",rep ,".RData"))
-
-  }
-  
+    save(est_model, file = paste0(path,"est_sim_EPI_", "_", par_mcrep, "_", par_nsplitmerge, "_", par_R,"_",rep ,".RData"))
+  } 
 }
 
 
